6202545c34ef58e3e0e5ec0f20b7691428dbaca9
x3l51 <31654136+x3l51@users.noreply.github.com>
2019-01-17 14:29:09 +0100

    Create network_scan.py

---
+++ network_scan.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# network_scan.py
+
+import datetime
+import subprocess
+from xml.dom import minidom
+
+subprocess.check_call("sudo nohup nmap -sP -sn -oX network_scan_" + str(datetime.datetime.now().strftime('%Y-%m-%d_%$
+doc = minidom.parse('network_scan.log')
+data = doc.getElementsByTagName('host')
+
+nmap = doc.getElementsByTagName('nmaprun')
+nmap_time = int(nmap[0].attributes['start'].value)
+date = datetime.datetime.fromtimestamp(nmap_time)
+print('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
+
+def func():
+    for i, v in enumerate(data):
+        items = v.getElementsByTagName('address')
+        itemsTwo = v.getElementsByTagName('hostname')
+        itemsThree = v.getElementsByTagName('hostname')
+
+        try:
+            IP_get = items[0].attributes['addr'].value
+        except:
+            IP_get = ('n/a')
+        print('IP: ' + IP_get)
+
+        try:
+            MAC_get = items[1].attributes['addr'].value
+        except:
+            MAC_get = ('n/a')
+        print('MAC: ' + MAC_get)
+
+        try:
+            VENDOR_get = items[1].attributes['vendor'].value
+        except:
+            VENDOR_get = ('n/a')
+        print('Vendor: ' + VENDOR_get)
+
+        try:
+            NAME_get = itemsTwo[0].attributes['name'].value
+        except:
+            NAME_get = ('n/a')
+        print('Name: ' + NAME_get)
+
+        print('\n')
+func()
