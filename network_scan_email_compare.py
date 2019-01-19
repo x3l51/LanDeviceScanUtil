55f93cf48c9ae80616d98e1e6ea660a250fbd5f3
x3l51 <31654136+x3l51@users.noreply.github.com>
2019-01-19 13:54:31 +0100

    Update network_scan_email_compare.py

--- network_scan_email_compare.py
+++ network_scan_email_compare.py
@@ -14,16 +14,18 @@ from email.mime.text import MIMEText
 import json
 from xml.dom import minidom
 
-timeNow = str(datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))
 doc = minidom.parse('network_scan.log')
 data = doc.getElementsByTagName('host')
 
 nmap = doc.getElementsByTagName('nmaprun')
 nmap_time = int(nmap[0].attributes['start'].value)
 date = datetime.datetime.fromtimestamp(nmap_time)
+timeNow = str(date.strftime('%Y-%m-%d_%H:%M:%S'))
 dataList = []
+dataListNew = []
 
 dataList.append('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
+dataListNew.append('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
 print('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
 
 try:
@@ -33,12 +35,15 @@ try:
     if response == 0:
         subprocess.check_call("sudo nmap -sP -sn -oX network_scan.log 192.168.0.* > /dev/null 2>&1", shell=True)
         dataList.append('Public IP: ' + public_ip + '\n')
+        dataListNew.append('Public IP: ' + public_ip + '\n')
         print('Public IP: ' + public_ip + '\n')
     else:
         dataList.append('Public IP: n/a (Bad domain? (' + hostname + ')?\n')
+        dataListNew.append('Public IP: n/a (Bad domain? (' + hostname + ')?\n')
         print('Public IP: n/a (Bad domain? (' + hostname + ')?\n')
 except:
     dataList.append('NO INTERNET CONNECTIVITY\n')
+    dataListNew.append('NO INTERNET CONNECTIVITY\n')
     print('NO INTERNET CONNECTIVITY\n')
 
     with open('network_scan_all.txt', 'w') as readable:
@@ -52,18 +57,18 @@ def func():
         itemsTwo = v.getElementsByTagName('hostname')
 
         try:
-            IP_get = items[0].attributes['addr'].value
-        except:
-            IP_get = ('n/a')
-        dataList.append('IP: ' + IP_get)
-        print('IP: ' + IP_get)
-
-        try:
             NAME_get = itemsTwo[0].attributes['name'].value
         except:
             NAME_get = ('n/a')
         dataList.append('Name: ' + NAME_get)
         print('Name: ' + NAME_get)
+
+        try:
+            IP_get = items[0].attributes['addr'].value
+        except:
+            IP_get = ('n/a')
+        dataList.append('IP: ' + IP_get)
+        print('IP: ' + IP_get)
 
         try:
             MAC_get = items[1].attributes['addr'].value
@@ -83,34 +88,42 @@ def func():
 
         with open('network_scan_all.json') as json_file:
             data_all = json.load(json_file)
-            LAST_SEEN_get = (data_all[MAC_get]["FIRST_SEEN"])
-            dataList.append('First seen: ' + LAST_SEEN_get)
-            print('First seen: ' + LAST_SEEN_get)
         
         if MAC_get in data_all:
             print('Status: Known Device.')
             dataList.append('Status: Known device.')
+            FIRST_SEEN_get = (data_all[MAC_get]["FIRST_SEEN"])
+            dataList.append('First seen: ' + FIRST_SEEN_get)
+            print('First seen: ' + FIRST_SEEN_get)
+            dataList.append('\n')
+            print('\n')
             SEEN_val = {'SEEN': timeNow}
             data_all[MAC_get].update(SEEN_val)
             with open('network_scan_all.json', 'w') as outfile:
                 json.dump(data_all, outfile)
         else:
             print('Status: Unknown device.')
+            dataListNew.append('Name: ' + NAME_get)
+            dataListNew.append('IP: ' + IP_get)
+            dataListNew.append('MAC: ' + MAC_get)
+            dataListNew.append('Vendor: ' + VENDOR_get)
             dataList.append('Status: Unknown device.')
+            dataListNew.append('Status: Unknown device.')
+            FIRST_SEEN_get = timeNow
+            dataListNew.append('First seen: ' + FIRST_SEEN_get)
+            dataListNew.append('\n')
+            dataList.append('\n')
             data_all[MAC_get] = DEVICE
             with open('network_scan_all.json', 'w') as outfile:
                 json.dump(data_all, outfile)
 
-        dataList.append('\n')
-        print('\n')
 
     with open('network_scan_all.txt', 'w') as readable:
         for item in dataList:
             readable.write("%s\n" % item)
 
-    if 'Unknown device.' in dataList:
+    if 'Status: Unknown device.' in dataList:
         sendMail()
-        return
     else:
         return
 
@@ -122,16 +135,16 @@ def sendMail():
     msg['From'] = fromaddr
     msg['To'] = toaddr
     msg['Subject'] = "Unknown device detected - network_scan_" + timeNow + ".log"
-    body = '\n'.join(dataList)
+    body = '\n'.join(dataListNew)
     msg.attach(MIMEText(body, 'plain'))
     
-    server = smtplib.SMTP('SMTP_SERVER_HERE', 587)
+    server = smtplib.SMTP('SERVER_HERE', 587)
     server.starttls()
     server.login(fromaddr, "PASSWORD_HERE")
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
-    print('Email got sent.\n')
+    print('\nEmail got sent.\n')
 
 func()
 
