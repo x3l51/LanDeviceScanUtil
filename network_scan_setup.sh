bef6099bad154777411157c9a32420f6e41e1b0e
x3l51 <31654136+x3l51@users.noreply.github.com>
2019-01-19 12:10:43 +0100

    Update network_scan_setup.sh

--- network_scan_setup.sh
+++ network_scan_setup.sh
@@ -4,8 +4,6 @@ cd ~/Network_Scan
 sudo wget https://github.com/x3l51/LanDeviceScanUtil/archive/master.zip
 sudo unzip master.zip
 cd LanDeviceScanUtil-master/
-sudo chmod +x network_scan.py
-sudo chmod +x network_scan_email.py
 sudo chmod +x network_scan_email_compare.py
 crontab -l > mycron
 echo "* * * * * cd /filePath/to/yourScript && python3 network_scan_email_compare.py" >> mycron
