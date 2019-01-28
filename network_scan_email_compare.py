e97f35afb262ab44a750728873fd68227e9406cb
x3l51 <31654136+x3l51@users.noreply.github.com>
2019-01-28 14:06:36 +0100

    Now shows more info and is more failsafe
    Now shows more info and is more failsafe

--- network_scan_email_compare.py
+++ network_scan_email_compare.py
@@ -2,21 +2,33 @@
 # -*- coding: utf-8 -*-
 # network_scan_email_compare.py
 
-import datetime
-import subprocess
-import smtplib
-import mimetypes
-import email
-import requests
-import os
-import os.path, time
-import sys
-import json
-from email import encoders
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from xml.dom import minidom
+try:
+    import datetime
+    import subprocess
+    import smtplib
+    import mimetypes
+    import email
+    import requests
+    import os
+    import os.path, time
+    import sys
+    import json
+    from email import encoders
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.base import MIMEBase
+    from xml.dom import minidom
+except ImportError:
+    subprocess.call("sudo -H apt-get install python3-pip -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3-nmap -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 datetime -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 subprocress -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 smtplib -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 mimetypes -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 email -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 requests -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 json -y > {}".format(os.devnull), shell=True)
+    subprocess.call("sudo -H apt-get install python3 xml.dom -y > {}".format(os.devnull), shell=True)
 
 date = datetime.datetime.now()
 timeNow = str(date.strftime('%Y-%m-%d_%H:%M:%S'))
@@ -35,77 +47,112 @@ dataListHTML.append('<html><head><meta http-equiv="refresh" content="60" charset
         .sticky {padding: 2px 33px; background: #555;color: #f1f1f1;position: fixed;top: 0;width: 100%;} \
         .sticky + .content {padding-top: 102px;} \
         table {width: 100%;border-collapse: collapse;} \
-        td, th { border: 1px solid #dddddd;text-align: left;padding: 8px;} \
-        </style></head><body style="background-color:#dddddd;"><div class="sticky" id="topHeader"><br>Time: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '<br>\n')
+        td { width: 50%; border: 1px solid #dddddd;text-align: left;padding: 8px;vertical-align: top;} \
+        th { border: 1px solid #dddddd;text-align: left;padding: 8px;vertical-align: top;} \
+        </style></head><body style="background-color:#dddddd;"><div class="sticky" id="topHeader"><br>Time: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '<br><br>\n')
 
 dataList.append('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
 dataListNew.append('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
 dataListAll.append('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
-print('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n')
+print('\nTime: ' + date.strftime('%Y-%m-%d %H:%M:%S') + '\n') 
 
-def errorHandle():
-    dataList.append('Public IP: n/a (Bad domain? (' + hostname + ') or NO INTERNET CONNECTIVITY or other error.\n')
-    dataListNew.append('Public IP: n/a (Bad domain? (' + hostname + ') or NO INTERNET CONNECTIVITY or other error.\n')
-    dataListAll.append('Public IP: n/a (Bad domain? (' + hostname + ') or NO INTERNET CONNECTIVITY or other error.\n')
-    dataListHTML.append('Public IP: n/a (Bad domain? (' + hostname + ') or NO INTERNET CONNECTIVITY or other error.\n')
-    print('Public IP: n/a (Bad domain? (' + hostname + ') or NO INTERNET CONNECTIVITY or other error.\n')
-    with open('network_scan_online.txt', 'w') as readable:
-        for item in dataList:
-            readable.write("%s\n" % item)
-        sys.exit()   
-
-try:
-    public_ip = requests.get('http://ip.42.pl/raw').text
-    hostname = "ip.42.pl"
-    response = os.system("ping -c 1 " + hostname + " > /dev/null 2>&1")
-    if response == 0:
-        stdoutdataName = subprocess.getoutput("hostname")
-        stdoutdataIP = subprocess.getoutput("ifconfig | grep \"inet \" | grep -v 127.0.0.1 | awk '{print $2}'")
-        stdoutdataMAC = subprocess.getoutput("cat /sys/class/net/*/address | awk 'NR==1{print $1}'")
-        stdoutdataMACDiff = stdoutdataMAC[:8].replace(":","-")
+public_ip = requests.get('http://ip.42.pl/raw').text
+hostname = "ip.42.pl"
+response = os.system("ping -c 1 " + hostname + " > /dev/null 2>&1")
+if response == 0:
+    stdoutdataName = subprocess.getoutput("hostname")
+    stdoutdataIP4loc = subprocess.getoutput("ifconfig | grep \"inet \" | grep -v 127.0.0.1 | awk '{print $2}'")
+    if stdoutdataIP4loc is "":
+        stdoutdataIP4loc = "n/a"
+    stdoutdataIP6loc = subprocess.getoutput("ifconfig | grep \"inet6 \" | grep fe | awk '{print $2}'")
+    if stdoutdataIP6loc is "":
+        stdoutdataIP6loc = "n/a"
+    stdoutdataIP6pub = subprocess.getoutput("ifconfig | grep \"inet6 \" | grep -v fe | grep -v ::")
+    if stdoutdataIP6pub is "":
+        stdoutdataIP6pub = "n/a"
+    stdoutdataIface = subprocess.getoutput("route | grep '^default' | grep -o '[^ ]*$'")
+    if stdoutdataIface.startswith('e'):
+        stdoutdataIface = ("Ethernet (" + stdoutdataIface + ")")
+    elif stdoutdataIface.startswith('e'):
+        stdoutdataIface = ("Wireless (" + stdoutdataIface + ")")
+    stdoutdataMAC = subprocess.getoutput("cat /sys/class/net/*/address | awk 'NR==1{print $1}'")
+    stdoutdataMACDiff = stdoutdataMAC[:8].replace(":","-")
+    try:
         stdoutdataVendor = subprocess.getoutput("grep -i \"" + stdoutdataMACDiff + "\" /var/lib/ieee-data/oui.txt | awk '{$1=$2=\"\"; print substr($0,2)}'")
-        subprocess.check_call("sudo nmap -sP -sn -oX network_scan_online.log 192.168.0.* > /dev/null 2>&1", shell=True)
-        dataList.append('Public IP: ' + public_ip + '\n')
-        dataListNew.append('Public IP: ' + public_ip + '\n')
-        dataListAll.append('Public IP: ' + public_ip + '\n')
-        dataListHTML.append('Public IP: ' + public_ip + '<br><br>\n')
-        print('Public IP: ' + public_ip + '\n')
-        if os.path.exists('network_scan_info.log'):
-            if time.time() - os.path.getmtime('network_scan_info.log') > (60 * 60):
-                subprocess.check_call("sudo nmap -F -A -oN network_scan_info.log " + public_ip + " > /dev/null 2>&1", shell=True)
-        else:
+    except:
+        stdoutdataVendor = "n/a"
+    subprocess.check_call("sudo nmap -sP -sn -oX network_scan_online.log 192.168.0.* > /dev/null 2>&1", shell=True)
+    
+    if os.path.exists('network_scan_info.log'):
+        if time.time() - os.path.getmtime('network_scan_info.log') > (60 * 60):
             subprocess.check_call("sudo nmap -F -A -oN network_scan_info.log " + public_ip + " > /dev/null 2>&1", shell=True)
-        with open('network_scan_info.log') as infoFile:
-            infoAll = infoFile.read()
-
-        if os.path.exists('network_scan_open_ports.txt'):
-            if time.time() - os.path.getmtime('network_scan_open_ports.txt') > (60 * 60):
-                subprocess.check_call("sudo nmap -F " + public_ip + " | grep open > network_scan_open_ports.txt", shell=True)
-        else:
-            subprocess.check_call("sudo nmap -F " + public_ip + " | grep open > network_scan_open_ports.txt", shell=True)
-
-        if not os.path.exists('network_scan_all.json'):
-            dummyData = "{\"" + stdoutdataMAC + "\": {\"FIRST_SEEN\": \"" + timeNow + "\",\"IP\": \"" + stdoutdataIP + "\",\"MAC\": \"" + stdoutdataMAC + "\",\"NAME\": \"" + stdoutdataName + "\",\"SEEN\": \"" + timeNow + "\",\"VENDOR\": \"" + stdoutdataVendor + "\"}}"
-            with open('network_scan_all.json', 'w') as outfile:
-                outfile.write(dummyData)
-
-        dataListHTML.append('</div><div class="content"><table><tr>')
-
-        with open('network_scan_open_ports.txt') as portFileHTML:
-            dataListHTML.append('<td><table><tr><tr><th>Open ports:</th></tr></tr><tr><td>')
-            for line in portFileHTML:
-                dataListHTML.append("%s<br>\n" % line)
-            dataListHTML.append('</td></tr></table></td>')
-        with open('network_scan_open_ports.txt') as portFile:
-            portAll = portFile.read()
-            dataList.append('Open ports:\n\n' + portAll + '\n')
-            dataListNew.append('Open ports:\n\n' + portAll + '\n')
-            dataListAll.append('Open ports:\n\n' + portAll + '\n')
-            print('Open ports:\n\n' + portAll + '\n')
     else:
-        errorHandle()
-except:
-    errorHandle()
+        subprocess.check_call("sudo nmap -F -A -oN network_scan_info.log " + public_ip + " > /dev/null 2>&1", shell=True)
+    with open('network_scan_info.log') as infoFile:
+        infoAll = infoFile.read()
+
+    if os.path.exists('network_scan_open_ports.txt'):
+        if time.time() - os.path.getmtime('network_scan_open_ports.txt') > (60 * 60):
+            subprocess.check_call("sudo nmap -F " + public_ip + " | grep open > network_scan_open_ports.txt", shell=True)
+    else:
+        subprocess.check_call("sudo nmap -F " + public_ip + " | grep open > network_scan_open_ports.txt", shell=True)
+
+    if not os.path.exists('network_scan_all.json'):
+        dummyData = "{\"" + stdoutdataMAC + "\": {\"FIRST_SEEN\": \"" + timeNow + "\",\"IP\": \"" + stdoutdataIP4loc + "\", \
+        \"IPv6loc\": \"" + stdoutdataIP6loc + "\",\"IPv6pub\": \"" + stdoutdataIP6pub + "\",\"MAC\": \"" + stdoutdataMAC + "\", \
+        \"NAME\": \"" + stdoutdataName + "\",\"SEEN\": \"" + timeNow + "\",\"VENDOR\": \"" + stdoutdataVendor + "\"}}"
+        with open('network_scan_all.json', 'w') as outfile:
+            outfile.write(dummyData)
+    
+    dataListHTML.append('</div><div class="content"><table><tr>')
+
+    print('IP Table of ' + stdoutdataName + ':\n')
+    print('IPv4 local: ' + stdoutdataIP4loc)
+    print('IPv4 public: ' + public_ip)
+    print('IPv6 local: ' + stdoutdataIP6loc)
+    print('IPv6 public: ' + stdoutdataIP6pub + '\n')
+    print('Interface: ' + stdoutdataIface + '\n')
+
+    dataList.append('IP Table of ' + stdoutdataName + ':\n')
+    dataList.append('IPv4 local: ' + stdoutdataIP4loc)
+    dataList.append('IPv4 public: ' + public_ip)
+    dataList.append('IPv6 local: ' + stdoutdataIP6loc)
+    dataList.append('IPv6 public: ' + stdoutdataIP6pub + '\n')
+    dataList.append('IPv6 public: ' + stdoutdataIP6pub + '\n')
+
+    dataListNew.append('IP Table of ' + stdoutdataName + ':\n')
+    dataListNew.append('IPv4 local: ' + stdoutdataIP4loc)
+    dataListNew.append('IPv4 public: ' + public_ip)
+    dataListNew.append('IPv6 local: ' + stdoutdataIP6loc)
+    dataListNew.append('IPv6 public: ' + stdoutdataIP6pub + '\n')
+    dataListNew.append('Interface: ' + stdoutdataIface + '\n')
+
+    dataListAll.append('IP Table of ' + stdoutdataName + ':\n')
+    dataListAll.append('IPv4 local: ' + stdoutdataIP4loc)
+    dataListAll.append('IPv4 public: ' + public_ip)
+    dataListAll.append('IPv6 local: ' + stdoutdataIP6loc)
+    dataListAll.append('IPv6 public: ' + stdoutdataIP6pub + '\n')
+    dataListAll.append('Interface: ' + stdoutdataIface + '\n')
+
+    dataListHTML.append('<td><table><tr><tr><th>IP Table of ' + stdoutdataName + ':</th></tr></tr><tr><td>\n')
+    dataListHTML.append('IPv4 local: ' + stdoutdataIP4loc + '<br>')
+    dataListHTML.append('IPv4 public: ' + public_ip + '<br><br>\n')
+    dataListHTML.append('IPv6 local: ' + stdoutdataIP6loc + '<br>')
+    dataListHTML.append('IPv6 public: ' + stdoutdataIP6pub + '<br><br>\n')
+    dataListHTML.append('Interface: ' + stdoutdataIface + '<br>\n')
+    dataListHTML.append('</td></tr></table></td>')
+
+    with open('network_scan_open_ports.txt') as portFileHTML:
+        dataListHTML.append('<td><table><tr><tr><th>Open ports:</th></tr></tr><tr><td>')
+        for line in portFileHTML:
+            dataListHTML.append("%s<br>\n" % line)
+        dataListHTML.append('</td></tr></table></td>')
+    with open('network_scan_open_ports.txt') as portFile:
+        portAll = portFile.read()
+        dataList.append('Open ports:\n\n' + portAll + '\n')
+        dataListNew.append('Open ports:\n\n' + portAll + '\n')
+        dataListAll.append('Open ports:\n\n' + portAll + '\n')
+        print('Open ports:\n\n' + portAll + '\n')
 
 doc = minidom.parse('network_scan_online.log')
 data = doc.getElementsByTagName('host')
@@ -123,25 +170,28 @@ def func():
             IP_get = ('n/a')
 
         # MAC
-        try:
-            MAC_get = items[1].attributes['addr'].value
-        except:
-            if itemsTwo[0].attributes['name'].value == stdoutdataName:
-                MAC_get = (stdoutdataMAC)
-            else:
-                MAC_get = ('n/a')
+        if IP_get == stdoutdataIP4loc:
+            MAC_get = (stdoutdataMAC)
+        else:
+            try:
+                MAC_get = items[1].attributes['addr'].value
+            except:
+                if itemsTwo[0].attributes['name'].value == stdoutdataName:
+                    MAC_get = (stdoutdataMAC)
+                else:
+                    MAC_get = ('n/a')
 
         # NAME
         try:
             try:
                 NAME_get = itemsTwo[0].attributes['name'].value
-                if MAC_get == stdoutdataMAC: #local
+                if MAC_get == stdoutdataMAC:
                     NAME_get = (stdoutdataName)
                 if NAME_get is "n/a":
                     stdoutdataArpName = subprocess.getoutput("arp " + IP_get + " | grep -v Address | awk '{print$1}'")
                     NAME_get = stdoutdataArpName
             except:
-                if MAC_get == stdoutdataMAC: #local
+                if MAC_get == stdoutdataMAC:
                     NAME_get = (stdoutdataName)
 
                 stdoutdataArpName = subprocess.getoutput("arp " + IP_get + " | grep -v Address | awk '{print$1}'")
